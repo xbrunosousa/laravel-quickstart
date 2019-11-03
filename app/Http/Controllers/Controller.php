031b006248441e9000b3e99f85766ad62149b294
Bruno Sousa <xbrunosousa@icloud.com>
2019-11-03 04:41:50 -0300

    :ok_hand: removed unused files and created jwt middleware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,50 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Dingo\Api\Routing\Helpers;
+use Mail;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Helpers;
+
+    public function success($message, $statusCode = 200)
+    {
+        return $this->response->array([
+            'success' => true,
+            'message' => $message,
+            'status' => $statusCode
+        ])->setStatusCode($statusCode);
+    }
+
+    public function error($message, $statusCode = 200)
+    {
+        return $this->response->array([
+            'success' => false,
+            'message' => $message,
+            'status' => $statusCode
+        ])->setStatusCode($statusCode);
+    }
+
+    public function successData($data, $statusCode = 200)
+    {
+        return $this->response->array([
+            'success' => true,
+            'data' => $data,
+            'status' => $statusCode
+        ])->setStatusCode($statusCode);
+    }
+
+    public function sendEmail($view, $viewParams, $to, $subject)
+    {
+        Mail::send(
+            $view,
+            $viewParams,
+            function ($mail) use ($to, $subject) {
+                $mail->from(env('MAIL_FROM_ADDRESS'), env('APP_NAME'));
+                $mail->to($to['email'], $to['name']);
+                $mail->subject($subject);
+            }
+        );
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+$api = app('Dingo\Api\Routing\Router');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$api->version('v1', ['namespace' => 'App\Http\Controllers'], function ($api) {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    $api->group(['prefix' => 'user'], function () use ($api) {
+        $api->post('', 'UserController@store');
+    });
 });
