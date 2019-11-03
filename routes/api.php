db5b766d60b30560ae09944d2126ed16496beee0
Bruno Sousa <xbrunosousa@icloud.com>
2019-11-03 17:20:41 -0300

    :boom: Registered new routes

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,12 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers'], function ($api) {
 
     $api->group(['prefix' => 'user'], function () use ($api) {
         $api->post('', 'UserController@store');
+        $api->post('/verify/{code}', 'UserController@verify');
+        $api->group(['prefix' => 'auth'], function () use ($api) {
+            $api->post('', 'UserController@login');
+            $api->put('/forgot', 'UserController@requestPasswordReset');
+            $api->put('/reset_pwd', 'UserController@resetPwd');
+
+        });
     });
 });
