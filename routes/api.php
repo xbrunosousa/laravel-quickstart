089c0aadbcfb038a507d136a5d1a278363131e21
Bruno Sousa <xbrunosousa@icloud.com>
2019-11-03 20:27:37 -0300

    :building_construction: Add method to update user

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,11 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', ['namespace' => 'App\Http\Controllers'], function ($api) {
 
     $api->group(['prefix' => 'user'], function () use ($api) {
+        $api->post('', 'UserController@store');
         $api->group(['middleware' => 'jwt.auth'], function () use ($api) {
+            $api->put('', 'UserController@update');
             $api->get('', 'UserController@my');
         });
-        $api->post('', 'UserController@store');
         $api->post('/verify/{code}', 'UserController@verify');
         $api->group(['prefix' => 'auth'], function () use ($api) {
             $api->post('', 'UserController@login');
